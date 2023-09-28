6dfd0a8a548a5dcf10af1d11f291aff82653756c
bhoomikapotu <potubhoomika@gmail.com>
2023-09-28 12:47:42 +0530

    push the letest changes

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,7 @@
 export { default as useAddItem } from "./useAddItem";
 export { default as useCart } from "./useCart";
 export { default as useRemoveItem } from "./useRemoveItem";
+
+
+print("hello world")
+print("useCart")
